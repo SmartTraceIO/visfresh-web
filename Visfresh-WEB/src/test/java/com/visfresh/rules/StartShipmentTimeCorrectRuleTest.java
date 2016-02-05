5e024e11353633a9ce56ba7d4989f556543c8ca1
Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
2016-02-05 17:52:36 +0200

    Unit test for Start Shipment time correct rule

--- Visfresh-WEB/src/test/java/com/visfresh/rules/StartShipmentTimeCorrectRuleTest.java
+++ Visfresh-WEB/src/test/java/com/visfresh/rules/StartShipmentTimeCorrectRuleTest.java
@@ -3,13 +3,20 @@
  */
 package com.visfresh.rules;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.visfresh.dao.TrackerEventDao;
 import com.visfresh.entities.Device;
 import com.visfresh.entities.Shipment;
 import com.visfresh.entities.ShipmentStatus;
-
+import com.visfresh.entities.TrackerEvent;
+import com.visfresh.entities.TrackerEventType;
+import com.visfresh.rules.state.DeviceState;
 /**
  * @author Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
  *
@@ -37,11 +44,38 @@ public class StartShipmentTimeCorrectRuleTest extends BaseRuleTest {
 
     @Test
     public void testAccept() {
+       final DeviceState state = new DeviceState();
+       final TrackerEvent e = createTrackerEvent();
 
+       //check not accept
+       assertFalse(rule.accept(new RuleContext(e, state)));
+
+       state.possibleNewShipment(shipment);
+       assertFalse(rule.accept(new RuleContext(e, state)));
+
+       state.setStartShipmentDate(null);
+       assertTrue(rule.accept(new RuleContext(e, state)));
     }
-
     @Test
     public void testHandle() {
+        final DeviceState state = new DeviceState();
+        final TrackerEvent e = createTrackerEvent();
 
+        state.possibleNewShipment(shipment);
+        state.setStartShipmentDate(null);
+
+        assertFalse(rule.handle(new RuleContext(e, state)));
+        assertNotNull(state.getStartShipmentDate());
+    }
+
+    /**
+     * @return
+     */
+    private TrackerEvent createTrackerEvent() {
+        final TrackerEvent e = new TrackerEvent();
+        e.setShipment(shipment);
+        e.setDevice(shipment.getDevice());
+        e.setType(TrackerEventType.AUT);
+        return context.getBean(TrackerEventDao.class).save(e);
     }
 }
