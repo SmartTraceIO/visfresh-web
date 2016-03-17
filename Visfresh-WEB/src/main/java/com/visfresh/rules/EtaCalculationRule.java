48fc5ce445e04d3b79c412801fb2667b313125e1
Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
2016-03-17 10:51:24 +0200

    ETA calculation rule has implemented

---
+++ Visfresh-WEB/src/main/java/com/visfresh/rules/EtaCalculationRule.java
@@ -0,0 +1,125 @@
+/**
+ *
+ */
+package com.visfresh.rules;
+
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.visfresh.dao.ShipmentDao;
+import com.visfresh.entities.Location;
+import com.visfresh.entities.Shipment;
+import com.visfresh.entities.TrackerEvent;
+import com.visfresh.utils.LocationUtils;
+
+/**
+ * @author Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
+ *
+ */
+@Component
+public class EtaCalculationRule implements TrackerEventRule {
+    private static final String NAME = "EtaCalculation";
+    private static final Logger log = LoggerFactory.getLogger(EtaCalculationRule.class);
+
+    /**
+     * 60 km/h as meters/milliseconds
+     */
+    private static final double V_60_KM_H = 60. * 1000 / (60 * 60 * 1000);
+
+    @Autowired
+    private AbstractRuleEngine engine;
+    @Autowired
+    private ShipmentDao shipmentDao;
+
+    /**
+     * Default constructor.
+     */
+    public EtaCalculationRule() {
+        super();
+    }
+
+    @PostConstruct
+    public void initalize() {
+        engine.setRule(NAME, this);
+    }
+
+    /* (non-Javadoc)
+     * @see com.visfresh.rules.TrackerEventRule#accept(com.visfresh.rules.RuleContext)
+     */
+    @Override
+    public boolean accept(final RuleContext context) {
+        final TrackerEvent e = context.getEvent();
+        final Shipment s = e.getShipment();
+
+        return s != null
+            && !s.hasFinalStatus()
+            && s.getShippedFrom() != null
+            && s.getShippedTo() != null
+            && e.getTime().after(s.getShipmentDate());
+    }
+
+    /* (non-Javadoc)
+     * @see com.visfresh.rules.TrackerEventRule#handle(com.visfresh.rules.RuleContext)
+     */
+    @Override
+    public boolean handle(final RuleContext context) {
+        final TrackerEvent e = context.getEvent();
+        final Shipment s = e.getShipment();
+        final Location loc = new Location(e.getLatitude(), e.getLongitude());
+
+        log.debug("Calculate ETA for shipment: " + s.getId());
+        final Date eta = estimateArrivalDate(s, loc, s.getShipmentDate(), e.getTime());
+        if (eta != null) {
+            s.setEta(eta);
+            updateEta(s, eta);
+            log.debug("ETA for shipment: " + s.getId() + " has updated to " + eta);
+        }
+
+        return false;
+    }
+    /**
+     * @param s shipment.
+     * @param eta ETA date.
+     */
+    protected void updateEta(final Shipment s, final Date eta) {
+        shipmentDao.updateEta(s, eta);
+    }
+    /**
+     * @param s shipment.
+     * @param currentLocation current shipment location.
+     * @param startDate start shipment date.
+     * @param currentTime current time.
+     * @return ETA
+     */
+    protected Date estimateArrivalDate(final Shipment s,
+            final Location currentLocation, final Date startDate, final Date currentTime) {
+        final long dt = currentTime.getTime() - startDate.getTime();
+        final Location from = s.getShippedFrom().getLocation();
+        final Location to = s.getShippedTo().getLocation();
+
+        //calculate speed.
+        final double pathDone = LocationUtils.getDistanceMeters(
+                currentLocation.getLatitude(), currentLocation.getLongitude(),
+                from.getLatitude(), from.getLongitude());
+
+        //v should be not less then 60km/h
+        final double v = Math.max(V_60_KM_H, pathDone / dt);
+
+        //calculate reminder distance
+        double reminder = LocationUtils.getDistanceMeters(
+                currentLocation.getLatitude(), currentLocation.getLongitude(),
+                to.getLatitude(), to.getLongitude());
+        reminder = Math.max(reminder - s.getShippedTo().getRadius(), 0);
+        if (reminder == 0) {
+            return new Date(currentTime.getTime());
+        }
+
+        return new Date(currentTime.getTime() + (long) (reminder / v));
+    }
+}
--- Visfresh-WEB/src/test/java/com/visfresh/rules/EtaCalculationRuleTest.java
+++ Visfresh-WEB/src/test/java/com/visfresh/rules/EtaCalculationRuleTest.java
@@ -1,33 +1,112 @@
 /**
  *
  */
-package com.visfresh.mpl.services.arrival;
+package com.visfresh.rules;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.visfresh.entities.Location;
 import com.visfresh.entities.LocationProfile;
 import com.visfresh.entities.Shipment;
 import com.visfresh.entities.ShipmentStatus;
+import com.visfresh.entities.TrackerEvent;
+import com.visfresh.rules.state.DeviceState;
 
 /**
  * @author Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
  *
  */
-public class DefaultArrivalEstimationCalculatorTest extends DefaultArrivalEstimationCalculator {
+public class EtaCalculationRuleTest extends EtaCalculationRule {
+    private final Map<Long, Date> etas = new HashMap<>();
+    private Shipment shipment;
 
     /**
      * Default constructor.
      */
-    public DefaultArrivalEstimationCalculatorTest() {
+    public EtaCalculationRuleTest() {
         super();
+    }
+
+    @Before
+    public void setUp() {
+        final Location from = new Location(-33.75471658008509, 150.67508697509766);
+        final Location to = new Location(-38.75471658008509, 158.67508697509766);
+
+        this.shipment = createShipment(from, to);
+        shipment.setId(7L);
+    }
+
+    @Test
+    public void testAccept() {
+        final TrackerEvent e = new TrackerEvent();
+        e.setShipment(shipment);
+        e.setLatitude(-33.0);
+        e.setLongitude(151.0);
+        e.setTime(new Date());
+
+        final RuleContext c = new RuleContext(e, new DeviceState());
+
+        //check accept ok
+        assertTrue(accept(c));
+
+        //check not accept without from location
+        final LocationProfile from = shipment.getShippedFrom();
+
+        shipment.setShippedFrom(null);
+        assertFalse(accept(c));
+        shipment.setShippedFrom(from);
+
+        assertTrue(accept(c));
+
+        //check not accept without to location
+        final LocationProfile to = shipment.getShippedTo();
+
+        shipment.setShippedTo(null);
+        assertFalse(accept(c));
+        shipment.setShippedTo(to);
+
+        assertTrue(accept(c));
+
+        //check not accept in final status
+        shipment.setStatus(ShipmentStatus.Arrived);
+        assertFalse(accept(c));
+
+        shipment.setStatus(ShipmentStatus.InProgress);
+        assertTrue(accept(c));
+
+        //check not accept if shipment date after tracker event date
+        final Date sd = shipment.getShipmentDate();
+        shipment.setShipmentDate(e.getTime());
+        assertFalse(accept(c));
+
+        shipment.setShipmentDate(sd);
+        assertTrue(accept(c));
+    }
+
+    @Test
+    public void testUpdateEtaForShipment() {
+        final TrackerEvent e = new TrackerEvent();
+        e.setTime(new Date());
+        e.setShipment(shipment);
+        e.setLatitude(-33.0);
+        e.setLongitude(151.0);
+
+        //test ok
+        assertFalse(handle(new RuleContext(e, new DeviceState())));
+        assertNotNull(shipment.getEta());
+        assertNotNull(etas.get(shipment.getId()));
     }
 
     @Test
@@ -83,7 +162,7 @@ public class DefaultArrivalEstimationCalculatorTest extends DefaultArrivalEstima
     private Shipment createShipment(final Location from, final Location to) {
         final Shipment s = new Shipment();
         s.setShipmentDescription("Test Shipment");
-        s.setShipmentDate(new Date());
+        s.setShipmentDate(new Date(System.currentTimeMillis() - 1000000l));
         s.setStatus(ShipmentStatus.InProgress);
 
         //add locations
@@ -104,8 +183,8 @@ public class DefaultArrivalEstimationCalculatorTest extends DefaultArrivalEstima
         return createLocation(loc.getLatitude(), loc.getLongitude());
     }
     /**
-     * @param lat
-     * @param lon
+     * @param lat latitude
+     * @param lon longitude
      * @return
      */
     protected LocationProfile createLocation(final double lat, final double lon) {
@@ -116,5 +195,12 @@ public class DefaultArrivalEstimationCalculatorTest extends DefaultArrivalEstima
         p.getLocation().setLatitude(lat);
         p.getLocation().setLongitude(lon);
         return p;
+    }
+    /* (non-Javadoc)
+     * @see com.visfresh.mpl.services.DefaultArrivalEstimationService#updateEta(com.visfresh.entities.Shipment, java.util.Date)
+     */
+    @Override
+    protected void updateEta(final Shipment s, final Date eta) {
+        etas.put(s.getId(), eta);
     }
 }
