22487e0cd1cd7176a878d0f71eca3d1b2825990d
vsoldatov1966 <vyacheslav.soldatov@bookedin.net>
2018-04-25 12:51:20 +0300

    Soft dispatcher stopping has added to bt04

---
+++ bt04/src/main/java/au/smarttrace/db/NamedParameterJdbcTemplateImpl.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package au.smarttrace.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
+ *
+ */
+@Component
+public class NamedParameterJdbcTemplateImpl extends NamedParameterJdbcTemplate {
+    /**
+     * @param dataSource
+     */
+    @Autowired
+    public NamedParameterJdbcTemplateImpl(final DataSourceTransactionManager mgr) {
+        super(mgr.getDataSource());
+    }
+}
